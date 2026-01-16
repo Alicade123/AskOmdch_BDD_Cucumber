2bb818c714bf4440d2dcdc84e696aa697ace1099
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-16 13:01:57 +0200

    Refactor UtilClass

--- src/test/java/dependency/injection/UtilClass.java
+++ src/test/java/dependency/injection/UtilClass.java
@@ -1,19 +1,7 @@
 package dependency.injection;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-
 public class UtilClass {
     public static String username;
-    public final String siteUrl = "https://askomdch.com/";
-    public WebDriver driver;
+    public static final String SITEURL = "https://askomdch.com/";
 
-    public WebDriver setUp(){
-        return driver = new ChromeDriver();
-    }
-
-    public WebDriver tearDown(){
-        return driver;
-    }
 }
