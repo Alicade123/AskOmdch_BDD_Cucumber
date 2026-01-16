1f38374a0be1dcc7881cf3b357c2ea33c6298102
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-16 13:02:02 +0200

    Improve navigation step definitions

--- src/test/java/step/definitions/navigation/NavigationStepDefinition.java
+++ src/test/java/step/definitions/navigation/NavigationStepDefinition.java
@@ -1,15 +1,15 @@
 package step.definitions.navigation;
 
+import dependency.injection.DriverFactory;
 import dependency.injection.UtilClass;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,20 +21,17 @@ import static org.junit.Assert.assertEquals;
 
 public class NavigationStepDefinition {
     private UtilClass utilClass;
-    private WebDriver driver ;
+    private WebDriver driver  = DriverFactory.getDriver();
     private WebDriverWait wait;
     public NavigationStepDefinition (UtilClass utilClass){
         this.utilClass = utilClass;
-    }
-    @Before
-    public void setUp(){
-        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
+
     @Given("I'm On any page of the AskOmDch website")
     public void openSite(){
-        driver.get(utilClass.siteUrl);
+        driver.get(UtilClass.SITEURL);
         driver.manage().window().maximize();
     }
     @When("I click on {string}")
@@ -45,10 +42,7 @@ public class NavigationStepDefinition {
     public void checkPage(String pageTitle){
     assertEquals(pageTitle, driver.getTitle());
     }
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
+
 
 
 }
